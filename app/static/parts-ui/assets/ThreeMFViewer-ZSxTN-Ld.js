c6b229f02cb39136eeb60208e71f92d595b7c87d
Francisco Quesada <fcoquesada@gmail.com>
2025-09-08 18:14:58 +1000

    docker issues

--- app/static/parts-ui/assets/ThreeMFViewer-ZSxTN-Ld.js
+++ app/static/parts-ui/assets/ThreeMFViewer-ZSxTN-Ld.js
@@ -1,4 +1,4 @@
-import{r as Or,j as Br}from"./index-DW1a2kaC.js";/**
+import{r as Or,j as Br}from"./index-DwjaDk6G.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
