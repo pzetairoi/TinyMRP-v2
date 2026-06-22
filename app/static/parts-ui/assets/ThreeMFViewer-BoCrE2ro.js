7d69fc4d63bac16d5ca12c4806c530c91a901e37
Francisco Quesada <fcoquesada@gmail.com>
2026-06-22 16:33:00 +1000

    more filter bug fixes

--- app/static/parts-ui/assets/ThreeMFViewer-BoCrE2ro.js
+++ app/static/parts-ui/assets/ThreeMFViewer-BoCrE2ro.js
@@ -1,4 +1,4 @@
-import{r as Xt,j as te}from"./index-BLnqRqxW.js";/**
+import{r as Xt,j as te}from"./index-Dhs31yK9.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
