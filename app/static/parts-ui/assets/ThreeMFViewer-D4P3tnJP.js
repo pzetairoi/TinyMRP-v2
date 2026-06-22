42d16e7c5e5862c8a683a002cda31c287c1ef472
Francisco Quesada <fcoquesada@gmail.com>
2026-06-23 09:39:16 +1000

    badge summaru fixes

--- app/static/parts-ui/assets/ThreeMFViewer-D4P3tnJP.js
+++ app/static/parts-ui/assets/ThreeMFViewer-D4P3tnJP.js
@@ -1,4 +1,4 @@
-import{r as Xt,j as te}from"./index-Dhs31yK9.js";/**
+import{r as Xt,j as te}from"./index-DoAUfHkz.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
